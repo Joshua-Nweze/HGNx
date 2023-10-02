73aa685c96c3be516bc70b25ebafd3cd34d074e6
Joshua <joshuanweze270@gmail.com>
2023-10-02 13:24:45 +0100

    increase upload size

--- Task_Five/routes/video.routes.js
+++ Task_Five/routes/video.routes.js
@@ -1,6 +1,25 @@
 import express from "express"
 import multer from "multer"
-let upload = multer({ dest: 'upload',  })
+// let upload = multer({ dest: 'upload',  })
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      // Specify the destination directory where uploaded files will be saved.
+      // You can provide an absolute path or a relative path to your project's root.
+      cb(null, 'upload/'); // Example: 'uploads/' directory in your project
+    },
+    filename: function (req, file, cb) {
+      // Customize the filename if needed (e.g., add a timestamp or use the original filename)
+      cb(null, file.originalname);
+    },
+  });
+  
+  const upload = multer({
+    storage: storage, // Set the storage configuration
+    limits: {
+      fileSize: 50 * 1024 * 1024, // Set the maximum file size (e.g., 50MB)
+    },
+  });
 
 import videoController from '../controller/video.controller.js'
 
